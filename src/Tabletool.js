94331ad3b5fabe1750d5fd678c15da4c23bc1b9d
DESKTOP-1BAA9UP\katen <nopphawit@mbc.co.th>
2018-04-30 22:45:12 +0700

    table tool v1

--- src/Tabletool.js
+++ src/Tabletool.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
-// import logo from './logo.svg';
-// import './App.css';
 
 class Tabletool extends Component {
 	constructor(props) {
@@ -16,22 +13,101 @@ class Tabletool extends Component {
 		this.onSelectChange = this.onSelectChange.bind(this)
 	 }
 
+	 startTime(){
+		setInterval(function(){ 
+			this.setState({
+				timeUse: this.state.timeUse + 1
+			})
+		}.bind(this), 1000);
+	}
+
+	onStart(){
+		this.setState({
+			visible : !this.state.visible
+		})
+		this.startTime();
+	}
 
 	onSelectChange(e){
+		
 		var selectId = e.target.id.replace("select", "");
 		selectId = parseInt(selectId);
 		var newSelectedNodes = [];
 		if(selectId == this.state.selectedNodes.length){
 			{/*push new dropdown value*/}
+			console.log("add")
 			newSelectedNodes = this.state.selectedNodes.concat([e.target.value])
+		}else if(selectId == this.state.selectedNodes.length - 1){
+			console.log("delete and add")
+			newSelectedNodes = this.state.selectedNodes.slice(0, selectId);
+			newSelectedNodes = newSelectedNodes.concat([e.target.value])
 		}else{
 			{/*delete dropdown values*/}
-			newSelectedNodes = this.state.selectedNodes.slice(0, selectId+1);
+			console.log("delete")
+			newSelectedNodes = this.state.selectedNodes.slice(0, selectId);
 		}
 
+		this.updateSelectedNode(newSelectedNodes);
+		var questionNo = this.state.questionNo;
+		var questionText = this.props.question[questionNo].text;
+		if(this.isConsequent(newSelectedNodes) && this.isCorrectAnswer(newSelectedNodes, questionText)){
+			if(questionNo == this.props.question.length-1){
+				this.end();
+			}
+			this.updateQuestionNo();
+		}
+	}
+	end(){
+		console.log("end");
+	}
+
+	updateQuestionNo(){
+		this.setState({
+			questionNo : ++this.state.questionNo
+		})
+	}
+	updateSelectedNode(newSelectedNodes){
 		this.setState({
 			selectedNodes : newSelectedNodes
-		})
+		});
+	}
+
+	isConsequent(updatedSelectedNode){
+		var nodes = this.props.data.children;
+		for(var tierIndex=0;tierIndex<updatedSelectedNode.length;tierIndex++){
+			var nodeIndex = updatedSelectedNode[tierIndex];
+			console.log("tierindex "+tierIndex + "nodes" + JSON.stringify(nodes) + " updateNodes "+JSON.stringify(updatedSelectedNode));
+			console.log(" current node "+nodes[nodeIndex].name + " nodeIndex "+nodeIndex + "updatedSelectedNode length" + updatedSelectedNode.length);
+
+			nodes = nodes[nodeIndex].children;
+		}
+
+		if(!nodes){
+			return true;
+		}
+		return false;
+	}
+
+	isCorrectAnswer(updatedSelectedNodes, currentAnswer){
+		var tierNodes = this.props.data.children;
+		var userAnswer = "";
+		for(var tierNo=0;tierNo<updatedSelectedNodes.length;tierNo++){
+			var nodeIndex = updatedSelectedNodes[tierNo];
+			var tierNodeName = tierNodes[nodeIndex].name;
+			(tierNo == updatedSelectedNodes.length-1)? userAnswer += tierNodeName : userAnswer += tierNodeName + ",";
+			tierNodes = tierNodes[nodeIndex].children;
+		}
+
+
+		currentAnswer = currentAnswer.replace(/ /g,'')
+		currentAnswer = currentAnswer.replace("{", "")
+		currentAnswer = currentAnswer.replace("}", "")
+		currentAnswer = currentAnswer.replace("-->", ",")
+
+		if(userAnswer === currentAnswer){
+			return true;
+		}
+		return false;
 	}
 
 	showAllDropdowns(){
@@ -53,7 +129,7 @@ class Tabletool extends Component {
 				);
 			}.bind(this));
 
-			if(!this.isConsequent()){
+			if(!this.isConsequent(this.state.selectedNodes)){
 				var newSelectList = [0].map(function(node) {
 				var selectId = "select" + this.state.selectedNodes.length;
 					return (
@@ -70,17 +146,6 @@ class Tabletool extends Component {
 		}
 	}
 
-	isConsequent(){
-		var allSelectNodes = this.state.selectedNodes;
-		var nodes = this.props.data.children;
-		for(var tierIndex=0;tierIndex<allSelectNodes.length;tierIndex++){
-			var nodeIndex = allSelectNodes[tierIndex];
-			nodes = nodes[nodeIndex].children;
-		}
-		if(!nodes){return true;}
-		return false;
-	}
-
 	getOptionList(tier){
 		var parentList = this.props.data.children;
 		
@@ -91,6 +156,7 @@ class Tabletool extends Component {
 			var parentNode = parentList[parentNodeNumber];
 			parentList = parentNode.children;
 		}
+		if(!parentList){return}
 		var optionList = parentList.map(function(node, index) {
 			if(this.state.selectedNodes[tier]==index){
 				return(<option value={index} selected>{node.name}</option>)
@@ -100,14 +166,29 @@ class Tabletool extends Component {
 			
 		}.bind(this));
 
+
 		return optionList;
 	}
 
 	render() {
 		return (
-		<div class="row pl-md-5 pr-md-5 pt-md-5">
-			{this.showAllDropdowns()}
-		</div>
+			<div>
+				<div class="row pl-md-5 pr-md-5 pt-md-5">
+					<div class="col-md-6 col-sm-6 col-xs-6">
+						<button type="button" class="btn btn-primary float-left" onClick={() => this.onStart()}>Start</button>
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-6">
+						<div class="float-right">เวลาที่ใช้ {this.state.timeUse} วินาที</div>
+					</div>
+				</div>
+				<div class="row pl-md-5 pr-md-5 pt-md-5">
+				</div>
+				<div class="row pl-md-5 pr-md-5 pt-md-5" style={{visibility: this.state.visible ? 'visible':'hidden'}}>
+					{this.showAllDropdowns()}
+					{this.state.selectedNodes}
+					question{this.state.questionNo}
+				</div>
+			</div>
 		);
 	}
 }
