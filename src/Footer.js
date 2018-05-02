150dc4570d473914ef40e432e2be82b850d559d7
DESKTOP-1BAA9UP\katen <nopphawit@mbc.co.th>
2018-05-03 00:06:22 +0700

    do dynamic footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,17 +4,35 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class Footer extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+		  style : "absolute"
+		}
+	 }
+	componentDidMount() {
+        window.addEventListener("click", this.updateFootter.bind(this));
+    }
+    updateFootter(){
+    	if(document.documentElement.scrollHeight > window.innerHeight){
+    		this.setState({
+				style : "relative"
+			})
+    	}else{
+    		this.setState({
+				style : "absolute"
+			})
+    	}
 
+    }
 	render() {
 		
 		
 		return (
-			<footer class="footer">
-				<div>
-					<p>Created by: Nopphawit Karunlanchakorn</p>
-	    			<i class="fa fa-facebook"></i>   
-				</div>
-			</footer>
+			<div class="footer" style={{position: this.state.style}}>
+				<p>Created by: Nopphawit Karunlanchakorn</p>
+    			<i class="fa fa-facebook"></i>   
+			</div>
 		);
 	}
 }
